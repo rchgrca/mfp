a8b5c96e3a5e17fddb87d5f65a52a58e9db1fb57
Rich Garcia <rchgrca@yahoo.com>
2017-07-22 08:24:14 -0700

    adds variable strings for repetitive classnames

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MyFitnessPal Coding Exercise
+
+* Approach
+  * code for mobile first
+  * use functional css approach to demonstrate maintenance
+    * use variables for repetitive classnames on repetitive elements
+  * create single JSON structure to feed Redux store
+  * use Redux store to mutate state for adding, removing food items
--- js/index.jsx
+++ js/index.jsx
@@ -9,18 +9,21 @@ export default class App extends Component {
   }
 
   render() {
-    let pie = model.pie
+    let pie = model.pie,
+    gridStyle = "sm-col sm-col-6",
+    arrowStyle = "absolute cursor"
+
     return (
         <div className="clearfix pl2 pr2">
             <h2 className="center">mycalpal</h2>
-            <section className="sm-col sm-col-6 main">
+            <section className={`${gridStyle} main`}>
                 <div className="relative arrow-container">
-                    <i className="absolute left-0 cursor fa fa-arrow-left" onClick="" aria-hidden="true"></i>
-                    <i className="absolute right-0 cursor fa fa-arrow-right" onClick="" aria-hidden="true"></i>
+                    <i className={`${arrowStyle} left-0 fa fa-arrow-left`} onClick="" aria-hidden="true"></i>
+                    <i className={`${arrowStyle} right-0 fa fa-arrow-right`} onClick="" aria-hidden="true"></i>
                 </div>
                 <div className="center">Mon, July 17, 2017</div>
             </section>
-            <section className="sm-col sm-col-6 visual">
+            <section className={`${gridStyle} visual`}>
                 <div className="center">Calories per Meal:</div>
                 <Pie data={pie} options={pie.options}/>
             </section>
