b23ae737c34d84ed43a0e618cb665622115a5685
Rich Garcia <rchgrca@yahoo.com>
2017-07-22 21:46:51 -0700

    adds support for fake data

--- js/index.jsx
+++ js/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import moment from 'moment';
 import model from './models/index';
 import { Pie } from 'react-chartjs-2';
 
@@ -11,50 +12,24 @@ export default class App extends Component {
   render() {
     let pie = model.pie,
     gridStyle = "sm-col sm-col-6",
-    arrowStyle = "absolute cursor"
+    arrowStyle = "absolute cursor",
+    date = "2017-07-16",
+    day = model[date],
+    meals = Object.keys(day)
 
     return (
         <div className="clearfix pl1 pr1">
             <h2 className="center">mycalpal</h2>
             <p className="center h5 italic">"{model.quote}"</p>
-            <section className={`${gridStyle} main`}>
+            <section className={`${gridStyle} pb2 main`}>
                 <div className="relative arrow-container">
-                    <i className={`${arrowStyle} left-0 fa fa-arrow-left`} onClick="" aria-hidden="true"></i>
-                    <i className={`${arrowStyle} right-0 fa fa-arrow-right`} onClick="" aria-hidden="true"></i>
+                    <i className={`${arrowStyle} fa fa-arrow-left left-0`} onClick="" aria-hidden="true"></i>
+                    <i className={`${arrowStyle} fa fa-arrow-right right-0`} onClick="" aria-hidden="true"></i>
                 </div>
                 <div className="day">
-                    <div className="center pb1 date">Sun, July 16, 2017</div>
+                    <div className="center mb2 date">{moment(date).format('MMMM Do, YYYY')}</div>
                     <div className="">
-                        <table className="mx-auto">
-                            <thead>
-                                <tr>
-                                    <td className=""><div className="">Breakfast</div></td>
-                                    <td className="border border-navy bg-white center"><div className="">Calories</div></td>
-                                    <td className="border border-navy bg-white center"><div className="">Carbs</div></td>
-                                    <td className="border border-navy bg-white center"><div className="">Fat</div></td>
-                                    <td className="border border-navy bg-white center"><div className="">Protein</div></td>
-                                    <td className="border border-navy bg-white center"><div className="">Sugar</div></td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td className="border border-white relative px1 py2"><div className="truncate" title="Starkist Solid White Albacore Tuna In Water">Starkist Solid White Albacore Tuna In Water</div> <i className="fa fa-trash-o absolute top-0 right-0 cursor" title="Delete" aria-hidden="true"/></td>
-                                    <td className="border border-white center">1,000 cals</td>
-                                    <td className="border border-white center">3 g</td>
-                                    <td className="border border-white center">0 g</td>
-                                    <td className="border border-white center">0 g</td>
-                                    <td className="border border-white center">2 g</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-white relative px1 py2"><div className="truncate" title="Starkist Solid White Albacore Tuna In Water">Starkist Solid White Albacore Tuna In Water</div> <i className="fa fa-trash-o absolute top-0 right-0 cursor" title="Delete" aria-hidden="true"/></td>
-                                    <td className="border border-white center">1,000 cals</td>
-                                    <td className="border border-white center">3 g</td>
-                                    <td className="border border-white center">0 g</td>
-                                    <td className="border border-white center">0 g</td>
-                                    <td className="border border-white center">2 g</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {this.getMeals(day, meals)}
                     </div>
                 </div>
             </section>
@@ -65,6 +40,50 @@ export default class App extends Component {
         </div>
     )
   }
+
+  getMeals(day, meals){
+      return meals.map((meal) => {
+          return (
+              <table className="mx-auto mb2">
+                  <thead>
+                      <tr>
+                          <td><div className="capitalize bold">{meal}</div></td>
+                          <td className="border border-navy bg-white center"><div className="">Calories</div></td>
+                          <td className="border border-navy bg-white center"><div className="">Carbs</div></td>
+                          <td className="border border-navy bg-white center"><div className="">Fat</div></td>
+                          <td className="border border-navy bg-white center"><div className="">Protein</div></td>
+                          <td className="border border-navy bg-white center"><div className="">Sugar</div></td>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {this.getFoodItems(day, meal)}
+                  </tbody>
+              </table>
+          )
+      })
+  }
+
+  getFoodItems(day, meal){
+      let styles = "border border-white center";
+
+      return day[meal].map((item) => {
+          return (
+              <tr>
+                  <td className="border border-white relative px1 py1">
+                      <div className="truncate mr2" title={item.food}>{item.food}</div>
+                      <i className="fa fa-trash-o absolute top-0 right-0 cursor" title="Delete"/>
+                  </td>
+                  <td className={styles}>{item.calories} cals</td>
+                  <td className={styles}>{item.carbs} g</td>
+                  <td className={styles}>{item.fat} g</td>
+                  <td className={styles}>{item.protein} g</td>
+                  <td className={styles}>{item.sugar} g</td>
+              </tr>
+          )
+      })
+  }
+
+
 }
 
 render(<App/>, document.getElementById('app'));
