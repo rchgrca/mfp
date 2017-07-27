63a6b64dd37fd95b80cf3abebc9a561451ae8503
Rich Garcia <rchgrca@yahoo.com>
2017-07-26 18:23:24 -0700

    removes navy from body tag, removes extra bg-darkblue classname in index.jsx, removes extra #000 in media query

--- js/index.jsx
+++ js/index.jsx
@@ -118,7 +118,7 @@ export default class App extends Component {
   getSubmitButton(meal){
       return (
           <tr>
-              <td className="btn-container right-align border-none" colSpan="6"><button className="btn mb1 bg-darkblue rounded px2 py1 cursor bg-darkblue white border-none button relative" value={meal} onClick={this.handleSubmit}>Add Food</button></td>
+              <td className="btn-container right-align border-none" colSpan="6"><button className="btn mb1 bg-darkblue rounded px2 py1 cursor white border-none button relative" value={meal} onClick={this.handleSubmit}>Add Food</button></td>
           </tr>
       )
   }
